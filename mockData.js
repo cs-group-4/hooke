d312b5d7d00fc2d0e906809956b7948259fa10a9
EmmaGodfrey <emmanuekgodfrey4@gmail.com>
2023-05-29 07:00:30 +0200

    Added My Profile and Notifications

---
+++ mockData.js
@@ -0,0 +1,54 @@
+const mockNotifications = [
+  {
+    sender: "System",
+    message: "Complete your profile to find better matches",
+    timestamp: "2023-05-27T10:30:00Z"
+  },
+  {
+    sender: "John Doe",
+    message: "Liked your profile",
+    timestamp: "2023-05-26T15:45:00Z"
+  },
+  {
+    sender: "Anne Lincoln",
+    message: "Liked your profile",
+    timestamp: "2023-05-25T18:20:00Z"
+  },
+  {
+    sender: "System",
+    message: "Congratulations! You've matched with John Doe",
+    timestamp: "2023-05-24T09:10:00Z"
+  },
+  {
+    sender: "Michael Smith",
+    message: "Sent you a message",
+    timestamp: "2023-05-23T14:55:00Z"
+  },
+  {
+    sender: "System",
+    message: "Someone viewed your profile",
+    timestamp: "2023-05-22T20:40:00Z"
+  },
+  {
+    sender: "Emma Johnson",
+    message: "Liked your profile",
+    timestamp: "2023-05-21T12:15:00Z"
+  },
+  {
+    sender: "System",
+    message: "Complete your profile to increase your chances of finding a match",
+    timestamp: "2023-05-20T17:30:00Z"
+  },
+  {
+    sender: "Sarah Williams",
+    message: "Sent you a message",
+    timestamp: "2023-05-19T11:25:00Z"
+  },
+  {
+    sender: "David Wilson",
+    message: "Liked your profile",
+    timestamp: "2023-05-18T16:50:00Z"
+  }
+];
+
+window.mockNotifications = mockNotifications;
